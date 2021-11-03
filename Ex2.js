668986339b3cc8cafebec643d524b56f014dcf0d
Crymison <gabrielpaleta@hotmail.com>
2021-11-03 19:50:20 -0300

    commit exercicio 2 Atividade 2

--- Ex2.js
+++ Ex2.js
@@ -13,19 +13,27 @@ function positive(number) {
 }
 
 function isOdd(number) {
-    // Implementar
+    const n = R.clone(number);
+    n.odd = n.value % 2 == 1;
+    return n;
 }
 
 function negative(number) {
-    // Implementar
+    const n = R.clone(number);
+    n.negative = n.value < 0;
+    return n;
 }
 
 function isZero(number) {
-    // Implementar
+    const n = R.clone(number);
+    n.zero = n.value == 0;
+    return n;
 }
 
 function isPrime(number) {
-    // Implementar
+    const n = R.clone(number);
+    n.prime = n.value % 1 == 0;
+    return n;    
 }
 
 function mapToNumberObject(num) {
